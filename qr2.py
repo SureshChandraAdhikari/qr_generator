046cafa877573b9cbedfec69c036d8cf9c0d631a
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-05-29 12:22:24 +0530

    first commit

---
+++ qr2.py
@@ -0,0 +1,13 @@
+import qrcode 
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_H,
+                box_size=10,
+                border=4)
+qr.add_data("https://github.com/SureshChandraAdhikari")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="red",back_color="blue")
+img.save("sureshgithub2.png")
